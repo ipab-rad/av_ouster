64283482509c3cfd6d7dadbbcfd85819caa7c301
hect95 <hector_cruz95@live.com>
2024-05-16 12:26:10 +0100

    Avoid override dev and runtime images

--- dev.sh
+++ dev.sh
@@ -5,11 +5,11 @@
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_ouster:latest \
+    -t av_ouster:latest-dev \
     -f Dockerfile --target dev .
 
 # Run docker image with local code volumes for development
 docker run -it --rm --net host \
     -v /dev/shm:/dev/shm \
     -v ./av_ouster_launch:/opt/ros_ws/src/av_ouster_launch \
-    av_ouster:latest
+    av_ouster:latest-dev
